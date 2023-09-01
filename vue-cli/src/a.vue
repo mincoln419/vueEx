09d51e8ac4c3a7adb60e5d6ac56d8c202c0bf54d
mingeon cho <mgcho@ideatec.co.kr>
2023-09-01 15:38:25 +0900

    vue-cli add

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>header</div>
+
+</template>
+
+<script>
+export default {
+//Javascript - 인스턴스 옵션
+    methods: {
+        addNum: function() {
+
+        }
+    }
+
+}
+</script>
+
+<style>
+/* css */
+</style>
